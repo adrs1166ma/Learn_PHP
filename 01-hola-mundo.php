6b7d64d3b59bc4ad25f3a29855667b29c1434d35
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-04-18 10:31:01 -0500

    base

---
+++ 01-hola-mundo.php
@@ -0,0 +1,20 @@
+<?php include 'includes/header.php'; ?>
+
+<?php echo "hola Mundo"; ?>
+
+<br>
+
+<?php echo("Hola Mundo");
+
+
+print("Hola Mundo");
+
+print "Hola Mundo";
+
+print_r("Hola Mundo");
+
+var_dump("Hola Mundo"); //info de arreglo
+
+
+
+include 'includes/footer.php'; ?>
